3e9a60e412500427209e53a009aed9d49872ccc2
moezzouari <moez@octoplus.tech>
2023-05-18 15:27:12 +0100

    Function TV An Multiple

--- test.php
+++ test.php
@@ -11,17 +11,36 @@
 <body>
     <?php
     // debut afficher chaine
-     echo "<h1>Bonjour</h1>" ;
+    echo "<h1>Bonjour</h1>";
 
-     //Fin afficher chaine
+    //Fin afficher chaine
 
-     //definir une constante avec php 
-     define("TVA",19);
-     echo "<br>".TVA ;
+    //definir une constante avec php 
+    define("TVA", 19);
+    echo "TVA : " . TVA . "<br>";
 
-     // declaration et affichage une variable
-     $a=10;
-     echo "<br>".$a;
+    // declaration et affichage une variable
+    $a = 10;
+    echo "Valeur A : " . $a . "<br>";
+
+    // declaration d'une variable global
+    $GLOBALS['b'] = 5;
+    echo "Valeur B : " . $b . "<br>";
+
+    function multip($nbr)
+    {
+
+        $resultat = $GLOBALS['b'] * $nbr;
+        return $resultat;
+    }
+
+    function calculttc($montantht)
+    {
+        $resultat = $montantht * (1 + TVA / 100); //1.19
+        return $resultat;
+    }
+    echo "Multiplication de " . $GLOBALS['b'] . " * 3 : " . multip(3) . "<br>";
+    echo "Calcul TTC : " . calculttc(1000);
     ?>
 </body>
 
