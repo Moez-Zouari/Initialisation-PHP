7648cc028698c35e8ecbbe7b44e1a5bd6a6c89ec
moezzouari <moez@octoplus.tech>
2023-05-18 14:33:07 +0100

    first commit

---
+++ test.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    // debut afficher chaine
+     echo "<h1>Bonjour</h1>" ;
+
+     //Fin afficher chaine
+
+     //definir une constante avec php 
+     define("TVA",19);
+     echo "<br>".TVA ;
+
+     // declaration et affichage une variable
+     $a=10;
+     echo "<br>".$a;
+    ?>
+</body>
+
+</html>
